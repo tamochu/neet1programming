bd3dea163288eb6d793731811be0c29b5491bc37
son <son@My-PC>
2016-01-23 20:32:44 +0900

    test2

--- neet1programming/src/com/ecb/tamochu/neet1programming/Exam2.java
+++ neet1programming/src/com/ecb/tamochu/neet1programming/Exam2.java
@@ -8,13 +8,39 @@ public class Exam2 {
 	public static void main(String[] args) {
 		// TODO fizzbuzz問題。
 		// Q1.1～100の数字を表示しなさい。表示は↓の関数を使う
-		System.out.println();
+		for (int i=1;i<101;i++)
+		System.out.println(i);
 
 		// Q2.1～100の数字を表示しつつ3の倍数なら「fizz」と表示しなさい。
+		for (int i=1;i<101;i++){
+			if (i%3 == 0){
+				System.out.println("fizz");
+			} else System.out.println(i);
+
+		}
+
 
 		// Q3.1～100の数字を表示しつつ5の倍数なら「buzz」と表示しなさい。
+		for (int i=1;i<101;i++){
+			if (i%5==0){
+				System.out.println("buzz");
+			}else{
+				System.out.println(i);
+			}
+		}
 
 		// Q4.1～100の数字を表示しつつ3の倍数なら「fizz」5の倍数なら「buzz」3と5の倍数なら「fizzbuzz」と表示しなさい。
+		for (int i=1;i<101;i++){
+			if (i%3==0 && i%5==0){
+				System.out.println("fizzbuzz");
+			}else if (i%3==0){
+				System.out.println("fizz");
+			}else if (i%5==0){
+				System.out.println("buzz");
+			}else{
+				System.out.println(i);
+			}
+		}
 
 	}
 
