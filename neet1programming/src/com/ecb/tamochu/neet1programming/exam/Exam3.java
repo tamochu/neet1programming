b354620660fe300ba92c8af9be641e6d96bb4039
tamochu.mobile <tamochu.mobile@gmail.com>
2016-01-23 20:04:56 +0900

    exam

---
+++ neet1programming/src/com/ecb/tamochu/neet1programming/exam/Exam3.java
@@ -0,0 +1,17 @@
+package com.ecb.tamochu.neet1programming.exam;
+
+public class Exam3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 配列の操作。表示
+		// 次の文字列を続けて表示せよ。
+		// ヒント System.out.print() を使うと表示の度改行されない。
+
+		char[] hello = {'H', 'e', 'l', 'l', 'o'};
+
+	}
+
+}
