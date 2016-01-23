074448484746ed0257583c5e89a46a6084a97b4f
tamochu.mobile <tamochu.mobile@gmail.com>
2016-01-23 22:21:41 +0900

    commit

--- neet1programming/src/com/ecb/tamochu/neet1programming/exam/Exam3.java
+++ neet1programming/src/com/ecb/tamochu/neet1programming/exam/Exam3.java
@@ -7,8 +7,8 @@ public class Exam3 {
 	 */
 	public static void main(String[] args) {
 		// TODO 配列の操作。表示
-		// 次の文字列を続けて表示せよ。
-		// ヒント System.out.print() を使うと表示の度改行されない。
+		// 次の文字列を続けて表示せよ
+		// ヒント System.out.print() を使うと表示の度改行されない
 
 		char[] hello = {'H', 'e', 'l', 'l', 'o'};
 
