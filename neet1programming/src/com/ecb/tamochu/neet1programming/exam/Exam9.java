4f4efc33d3d938c70770890207b97189f3dc9c8b
son <son@My-PC>
2016-01-25 02:01:36 +0900

    よくわかりません

--- neet1programming/src/com/ecb/tamochu/neet1programming/exam/Exam9.java
+++ neet1programming/src/com/ecb/tamochu/neet1programming/exam/Exam9.java
@@ -1,6 +1,14 @@
 package com.ecb.tamochu.neet1programming.exam;
 
 public class Exam9 {
+	String name;
+	int age;
+	public void setName(String newname){
+		name = newname;
+	}
+	public String getName(){
+		return name;
+	}
 
 	/**
 	 * @param args
@@ -11,7 +19,24 @@ public class Exam9 {
 		// getNameメソッド（関数）から取得できるようにすること
 		// またsetNameメソッド（関数）からname変数を変更できるようにすること
 		// クラス名はExam9のままでよい
+				
+		Exam9[] persons = new Exam9[2];
+		
+		Exam9 takesi = new Exam9();
+		takesi.name = "takesi";
+		persons[0] = takesi;
+		
+		Exam9 john = new Exam9();
+		john.name = "john";
+		persons[1] = john;
+		
+		hyouji(persons);
 
+	}
+	public static void hyouji (Exam9[] persons){
+		for (Exam9 person : persons){
+			System.out.println(person.getName());
+		}
 	}
 
 }
