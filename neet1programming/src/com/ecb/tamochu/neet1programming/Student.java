259bfc1207232d95621ac9c9dc5b0ce1aee165fe
tamochu.mobile <tamochu.mobile@gmail.com>
2016-01-24 21:01:17 +0900

    students

---
+++ neet1programming/src/com/ecb/tamochu/neet1programming/Student.java
@@ -0,0 +1,44 @@
+package com.ecb.tamochu.neet1programming;
+
+public class Student {
+	String name;
+	int grade;
+
+	String getName() {
+		return "My name is " + name;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Student[] students = new Student[3];
+
+		Student takesi = new Student();
+		takesi.name = "takesi";
+		takesi.grade = 1;
+		students[0] = takesi;
+
+		Student john = new Student();
+		john.name = "john";
+		john.grade = 2;
+		students[1] = john;
+
+		Student suzuki = new Student();
+		suzuki.name = "suzuki";
+		suzuki.grade = 3;
+		students[2] = suzuki;
+
+		shoukai(students);
+	}
+
+	/**
+	 * 自己紹介の関数。
+	 * @param students
+	 */
+	public static void shoukai(Student[] students) {
+		for (Student student : students) {
+			System.out.println(student.getName());
+		}
+	}
+}
